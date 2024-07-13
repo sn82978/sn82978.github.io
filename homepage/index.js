53bd8eb43cd61915400a85b59d9192b7bb35532d
Shreya Nakum <shreyanakum2@gmail.com>
2024-07-12 21:04:24 -0700

    Finished homepage, missing styling. Created empty resume.html and did organization

---
+++ homepage/index.js
@@ -0,0 +1,61 @@
+function Header() {
+    return (
+        <header className="header">
+            <h1>Shreya Nakum</h1>
+            <nav className="nav"> 
+                <ul className="nav-items">
+                    <li><a href = "/">home</a></li>
+                    <li><a href = "resume.html">resume</a></li>
+                    <li>projects</li>
+                    <li>personal</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function MainContent() {
+    return (
+        <div>
+            <div className="intro-wrapper">
+                <img src="/homepage/shreya-head.JPG" className="shreya-pic" alt="Shreya" />
+                <div className="about-section">
+                        <h2>ABOUT</h2>
+                        <p>
+                            Welcome to my website! I am a dedicated computer science major specializing in Artificial Intelligence and Machine Learning at UC Irvine. As a member of the prestigious Campus Honors Collegium, I am committed to academic excellence and continuous learning. My passion for technology drives me to explore the latest advancements in AI and machine learning, ensuring that I stay at the forefront of this rapidly evolving field.
+                        </p>
+                        <p>
+                            Throughout my academic journey, I have gained valuable research experience, working on various projects that have honed my skills and deepened my understanding of machine learning algorithms and their applications. I am proficient in multiple programming languages, which allows me to tackle diverse challenges and develop innovative solutions. Whether collaborating on research initiatives or building cutting-edge applications, I am always eager to leverage my knowledge and skills to contribute meaningfully to the tech community.
+                        </p>
+                        <p>
+                            <b>Email</b>: snakum@uci.edu
+                        </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <ul className="logo-items">
+                <li><a href="https://www.linkedin.com/in/shreya-nakum-sn185/"><img src="/homepage/linkedin-logo.png" className="nav-logo" alt="LinkedIn Logo" /></a></li>
+                <li><a href="https://github.com/sn82978"><img src="/homepage/github-logo.png" className="nav-logo" alt="GitHub Logo" /></a></li>
+                <li><a href="mailto:snakum@uci.edu"><img src="/homepage/email-logo.png" className="nav-logo" alt="Email Logo" /></a></li>
+            </ul>
+        </footer>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
