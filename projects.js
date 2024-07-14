0d0a9e4c674c2e6234e50ac19d474787109c2e6b
Shreya Nakum <shreyanakum2@gmail.com>
2024-07-14 14:06:07 -0700

    reverted back the projects page

--- projects.js
+++ projects.js
@@ -40,8 +40,10 @@ function MainContent() {
   return (
     <div className="projects-container">
       <div className="projects-text">
-      <a href="iryna-research.html"><img src="/poster-presenting.jpg" className="project-pic" alt="Presenting Poster" /></a>
+      {/* <a href="iryna-research.html"><img src="/poster-presenting.jpg" className="project-pic" alt="Presenting Poster" /></a> */}
         <h3>Constraining cosmic ray transport with observations of the circumgalactic medium</h3>
+          <p>On the project, I wrote programs to create and analyze trends in plots and presenting the research. Initially, I wrote scripts in python using modules like numpy and pandas to do calculations with galaxy datasets. The dataset provided me with the galaxy’s stellar mass, virial radius, instantaneous star formation rate, and hydrogen column density. However, the model to calculate the effective cosmic ray transport rate used the variables of halo mass, circular velocity, and specific star formation rate. Thus, I converted the data into the variables I needed. The plots I created displayed the lower limits of the effective transport rate for each galaxy in the dataset as well as the errors present.</p>
+          <p>Next, I created a poster about the research and presented it at the AAS 240 conference. After the presentation, I began to work on the research paper. I helped write the methods section of the paper, describing the plot that displays the final transport rate values for each galaxy in the dataset.</p>
       </div>
       <div className="projects-text">
         <h3>Cipherbot: A Discord bot for the Science Olympiad Codebusters Event</h3>
