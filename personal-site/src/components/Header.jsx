6a91d6f57440bf4f4aec1dba377857df33799841
Shreya Nakum <shreyanakum2@gmail.com>
2024-12-24 13:30:08 -0800

    image fix

--- personal-site/src/components/Header.jsx
+++ personal-site/src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
  return (
    <header className="header">
      <div className="header-content">
-       <img src="src/assets/shreya-standing.png" alt="shreya standing" className="logo" />
+       <img src="public/shreya-standing.png" alt="shreya standing" className="logo" />
        <h4>shreya nakum</h4>
      </div>
    </header>
