93bbee084df100a1fedaf9fdb097f464f1a2d265
Shreya Nakum <shreyanakum2@gmail.com>
2025-03-12 16:38:17 -0700

    personal-site/src/components/Navigation.jsx

--- personal-site/src/components/Navigation.jsx
+++ personal-site/src/components/Navigation.jsx
@@ -9,7 +9,7 @@ function Navigation() {
        <li><Link to="/">home</Link></li>
        <li><Link to="/research">research</Link></li>
        <li><Link to="/projects">projects</Link></li>
-       <li><a href="https://drive.google.com/file/d/1cpp91v1sjnV_laQONBvLemMJBI1P8nb-/view?usp=sharing" target="_blank" rel="noopener noreferrer">resume</a></li>
+       <li><a href="https://drive.google.com/file/d/12f1we87TBJ78daKGBmC6RHoP3fVy-gO6/view?usp=sharing" target="_blank" rel="noopener noreferrer">resume</a></li>
      </ul>
    </nav>
  );
